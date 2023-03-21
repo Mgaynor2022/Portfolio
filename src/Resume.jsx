58d159d7f693e8bc9fb7a4485570b2df590cdb13
Michael Gaynor <mgaynor228@gmail.com>
2023-03-21 13:13:20 -0500

    updating resumePage

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -8,7 +8,7 @@ export default function Resume(){
                 <img src="images/resume.png" class="w-1/2"  />
             </div>
 
-                <a href='images/resume.png' download={"SoftwareEngineerResume.pdf"}>
+                <a href='https://github.com/Mgaynor2022/Mgaynor2022.github.io/blob/main/src/resume/SoftwareEngineerResume.pdf' download={"SoftwareEngineerResume.pdf"}>
                     <button type="button" class="mt-5 text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Download Resume</button>
                 </a>
 
