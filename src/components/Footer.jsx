23b5834fc78c7856df40790012b7fdab2fab87c1
Michael Gaynor <mgaynor228@gmail.com>
2023-05-27 17:40:59 -0500

    updating resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
                 <a href='https://www.linkedin.com/in/mgaynor228/' rel="noopener noreferrer nofollow" target='_blank'> <TfiLinkedin className='hover:text-gray-600 transition delay-200' color='black' size='2.5em'/> </a>
                 <a className='ml-5' href='https://github.com/Mgaynor2022' rel="noopener noreferrer nofollow" target='_blank'> <FaGithub className='hover:text-gray-600 transition delay-200'  color='black' size='2.5em'/> </a>
                 <a className='mx-4' 
-                    href='https://github.com/Mgaynor2022/Portfolio/blob/main/src/resume/VSchool%20Resume.pdf'
+                    href='https://github.com/Mgaynor2022/Portfolio/blob/main/src/resume/Michael_Gaynor_Resume_2023.pdf'
                     target="_blank" rel="noopener noreferrer nofollow">  <FiDownload className='hover:text-gray-600 transition delay-200' color='black' size='2.5em'/></a>
                     
             </div>
