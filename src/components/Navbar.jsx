1cddbc154b48996f58780c4237fb9afe56318227
Michael Gaynor <mgaynor228@gmail.com>
2023-05-27 17:02:52 -0500

    update mobile style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,12 +37,12 @@ export default function Navbar(){
     return(	
 
 		<div id=''className='transparent absolute z-50'>
-			<nav id="mobile-nav" className={mobileNavbar ? "bg-gray-100 opacity-75 w-screen fixed " : "hidden fixed w-screen transparent"}>
-				<div className={mobileNavBg ? "flex flex-wrap justify-between items-center p-4 bg-gray-100 opacity-75 w-screen": "flex flex-wrap justify-between items-center p-4 w-screen"}>
+			<nav id="mobile-nav" className={mobileNavbar ? "bg-white w-screen fixed " : "hidden fixed w-screen "}>
+				<div className={mobileNavBg ? "flex flex-wrap justify-between items-center p-4 bg-white w-screen": "flex flex-wrap justify-between items-center p-4 w-screen"}>
 					<h1> michaelPortfolio()</h1>
-					<RxHamburgerMenu className='mr-2 text-gray-600' onClick={toggle} size='2em' cursor='pointer'/>
+					<RxHamburgerMenu className='mr-2 ' onClick={toggle} size='2em' cursor='pointer'/>
 
-    <div className={mobileNavbar ? "flex w-screen z-50 bg-gray-100" : "hidden"} id="navbar-hamburger">
+    <div className={mobileNavbar ? "flex w-screen z-50 " : "hidden"} id="navbar-hamburger">
       <ul className="flex flex-col rounded-lg">
         <li className='mt-5 hover:text-black text-gray-800 transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8'>
           <Link onClick={toggle} to="#about" smooth aria-current="page">About</Link>
@@ -67,19 +67,19 @@ export default function Navbar(){
 	<nav id='bigNavbar' className='transparent '>
 		<ul id={navbar ? 'navbar-active' : 'navbar'} className="w-full h-20 flex fixed items-center text-white ">
 			<h1 className='text-black ml-5'> michaelPortfolio() </h1>
-			<Link className=' uppercase mx-4 hover:text-black text-gray-600 transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8' to="#about" smooth >About</Link>
+			<Link className=' uppercase mx-4 text-black transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8' to="#about" smooth >About</Link>
 				<li>
-					<Link className=' uppercase mx-4 hover:text-black text-gray-600 transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8' to="#projects" smooth >Projects</Link>
+					<Link className=' uppercase mx-4 text-black transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8' to="#projects" smooth >Projects</Link>
 				</li>
 				<li>
-					<a className=' uppercase mx-4 hover:text-black text-gray-600 transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8' 
+					<a className=' uppercase mx-4 text-black transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8 ' 
 						href='src/resume/michaelGaynorResume.pdf'
 						target="_blank" rel="noopener noreferrer">Resume
 					</a>
 					</li>
 						<li>
 						</li>
-						<Link className=' uppercase mx-4 hover:text-black text-gray-600 transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8' to="#contact" smooth >Contact</Link>
+						<Link className=' uppercase mx-4 text-black transition delay-150 hover:underline hover:decoration-4 hover:underline-offset-8' to="#contact" smooth >Contact</Link>
 					</ul>
 				</nav>
 			</div>
